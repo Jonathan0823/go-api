10aa9082f82e520415b20e21bef9aa2dc1624625
Guinn <eguinjonathan08@gmail.com>
2024-11-17 15:02:54 +0700

    add POST endpoint for creating a book with title and author validation

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
   r.GET("/", rootHandler)
   r.GET("/books/:id", getbook)
   r.GET("/books", getBookbyQuery)
+  r.POST("/books", postBook)
 
   fmt.Println("Server is running at localhost:8080")
   http.ListenAndServe("localhost:8080", r)
@@ -34,6 +35,27 @@ func getBookbyQuery(c *gin.Context){
   author := c.Query("author")
   c.JSON(http.StatusOK, gin.H{
     "message": "You requested to get a book with title: " + title + " and author: " + author,
+  })
+}
+
+type BookInput struct {
+  Title string `json:"title" binding:"required"`
+  Author string `json:"author" binding:"required"`
+}
+
+func postBook(c *gin.Context){
+ var input BookInput
+ err:= c.ShouldBindJSON(&input)
+ if err != nil {
+    c.JSON(http.StatusBadRequest, gin.H{
+      "error": err.Error(),
+    })
+    return
+  }
+
+  c.JSON(http.StatusOK, gin.H{
+    "title": input.Title,
+    "author": input.Author,
   })
 }
 
